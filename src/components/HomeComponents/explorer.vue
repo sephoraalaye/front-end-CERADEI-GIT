13ddb132f473f2110be03bcb57cbd2be5092d4f7
sephoraalaye <alayesephora99@gmail.com>
2023-12-02 10:49:00 +0100

    explore

--- src/components/HomeComponents/explorer.vue
+++ src/components/HomeComponents/explorer.vue
@@ -2,11 +2,11 @@
     
     <v-container style="margin-top: 10%">
         <v-row >
-            <v-col :cols="8" class="border "  >
+            <v-col :cols="10" class="border "  >
              <v-row class="d-flex " style="margin-bottom: 10%;" >
                 <v-card
                 class="mx-auto"
-                max-width="350"
+                max-width="400"
                 height="430"
             >
                 <v-img
@@ -35,67 +35,7 @@
                 </v-card>
                 <v-card
                 class="mx-auto"
-                max-width="350"
-                height="430"
-            >
-                <v-img
-                class="align-end text-white"
-                height="300"
-                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                cover
-                >
-                </v-img>
-
-                <v-card-subtitle class="pt-4" style="font-family: 'Poppins', sans-serif;font-weight: 500;font-size: 18px;line-height: 22.41px; color: rgba(94, 98, 130, 1);">
-                    Journée du Monde Professionnel (JMP)
-                </v-card-subtitle>
-
-                <div class="d-flex justify-space-between pa-3" style="font-family: 'Poppins', sans-serif;font-weight: 500;font-size: 16px;line-height: 19.92px; color:rgba(94, 98, 130, 1);">
-                    <div class="d-flex" >
-                        <div>
-                            <v-icon style="width: 20px;height: 20px;">mdi-calendar</v-icon>
-                        </div>
-                        <div >Avril 2022</div>
-                    </div>
-                    <div>
-                        <a href="" class="" style="text-decoration: none; color:rgba(94, 98, 130, 1);">Voir plus...</a>
-                    </div>
-                </div>
-                </v-card>
-            </v-row> 
-             <v-row class="d-flex" style="margin-bottom: 10%;" >
-                <v-card
-                class="mx-auto"
-                max-width="350"
-                height="430"
-            >
-                <v-img
-                class="align-end text-white"
-                height="300"
-                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                cover
-                >
-                </v-img>
-
-                <v-card-subtitle class="pt-4" style="font-family: 'Poppins', sans-serif;font-weight: 500;font-size: 18px;line-height: 22.41px; color: rgba(94, 98, 130, 1);">
-                    Journée du Monde Professionnel (JMP)
-                </v-card-subtitle>
-
-                <div class="d-flex justify-space-between pa-3" style="font-family: 'Poppins', sans-serif;font-weight: 500;font-size: 16px;line-height: 19.92px; color:rgba(94, 98, 130, 1);">
-                    <div class="d-flex" >
-                        <div>
-                            <v-icon style="width: 20px;height: 20px;">mdi-calendar</v-icon>
-                        </div>
-                        <div >Avril 2022</div>
-                    </div>
-                    <div>
-                        <a href="" class="" style="text-decoration: none; color:rgba(94, 98, 130, 1);">Voir plus...</a>
-                    </div>
-                </div>
-                </v-card>
-                <v-card
-                class="mx-auto"
-                max-width="350"
+                max-width="400"
                 height="430"
             >
                 <v-img
@@ -126,7 +66,7 @@
              <v-row class="d-flex" style="margin-bottom: 10%;" >
                 <v-card
                 class="mx-auto"
-                max-width="350"
+                max-width="400"
                 height="430"
             >
                 <v-img
@@ -155,7 +95,67 @@
                 </v-card>
                 <v-card
                 class="mx-auto"
-                max-width="350"
+                max-width="400"
+                height="430"
+            >
+                <v-img
+                class="align-end text-white"
+                height="300"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                cover
+                >
+                </v-img>
+
+                <v-card-subtitle class="pt-4" style="font-family: 'Poppins', sans-serif;font-weight: 500;font-size: 18px;line-height: 22.41px; color: rgba(94, 98, 130, 1);">
+                    Journée du Monde Professionnel (JMP)
+                </v-card-subtitle>
+
+                <div class="d-flex justify-space-between pa-3" style="font-family: 'Poppins', sans-serif;font-weight: 500;font-size: 16px;line-height: 19.92px; color:rgba(94, 98, 130, 1);">
+                    <div class="d-flex" >
+                        <div>
+                            <v-icon style="width: 20px;height: 20px;">mdi-calendar</v-icon>
+                        </div>
+                        <div >Avril 2022</div>
+                    </div>
+                    <div>
+                        <a href="" class="" style="text-decoration: none; color:rgba(94, 98, 130, 1);">Voir plus...</a>
+                    </div>
+                </div>
+                </v-card>
+            </v-row> 
+             <v-row class="d-flex" style="margin-bottom: 10%;" >
+                <v-card
+                class="mx-auto"
+                max-width="400"
+                height="430"
+            >
+                <v-img
+                class="align-end text-white"
+                height="300"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                cover
+                >
+                </v-img>
+
+                <v-card-subtitle class="pt-4" style="font-family: 'Poppins', sans-serif;font-weight: 500;font-size: 18px;line-height: 22.41px; color: rgba(94, 98, 130, 1);">
+                    Journée du Monde Professionnel (JMP)
+                </v-card-subtitle>
+
+                <div class="d-flex justify-space-between pa-3" style="font-family: 'Poppins', sans-serif;font-weight: 500;font-size: 16px;line-height: 19.92px; color:rgba(94, 98, 130, 1);">
+                    <div class="d-flex" >
+                        <div>
+                            <v-icon style="width: 20px;height: 20px;">mdi-calendar</v-icon>
+                        </div>
+                        <div >Avril 2022</div>
+                    </div>
+                    <div>
+                        <a href="" class="" style="text-decoration: none; color:rgba(94, 98, 130, 1);">Voir plus...</a>
+                    </div>
+                </div>
+                </v-card>
+                <v-card
+                class="mx-auto"
+                max-width="400"
                 height="430"
             >
                 <v-img
@@ -185,7 +185,7 @@
             </v-row> 
 
             </v-col>
-            <v-col :cols="4" class="">
+            <v-col :cols="2" class="">
                 <div class="border">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid rerum minima minus deleniti ullam? Laudantium iste, hic perspiciatis sit quidem aut atque optio quos reprehenderit temporibus corrupti ex asperiores ea.
                 </div>
